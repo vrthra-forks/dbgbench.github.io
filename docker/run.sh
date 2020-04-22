92a5c2a1ccc906a0de7685732461832b78a7e8e6
Rahul Gopinath <rahul@gopinath.org>
2020-04-22 22:59:11 +0200

    ddset

--- docker/run.sh
+++ docker/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <subject>"
+if [ $# -lt 2 ]; then
+  echo "Usage: $0 <subject> <name>"
   exit 1
 fi
 subject=$1
+name=$2
 
 if [ -z "$(which docker)" ]; then
   echo "Install docker from http://docs.docker.com/engine/installation/"
@@ -13,42 +14,47 @@ if [ $(docker images | grep -c "CREATED") -eq 0 ]; then
   echo "Run boot2docker to set up docker environment."
   exit 1
 fi
-if [ $(docker images | grep -c "debugstudy ") -eq 0 ]; then
+if [ $(docker images | grep -c "debugstudy$subject ") -eq 0 ]; then
   echo "Installing image.. This will be done only once and may take up to one hour."
-  docker build -t debugstudy -f scripts/docker.$subject . || exit 1
+  docker build -t debugstudy$subject -f scripts/docker.$subject . || exit 1
 fi
 
 exit_code=0
 
-if [ $(docker ps | grep -c "debugstudy ") -ne 0 ]; then
-  echo "An instance of 'debugstudy' is already running ($(docker ps | grep "debugstudy " | cut -c-12))"
-  echo "You can use VNCViewer from your Desktop or a different terminal window to access: "
-  echo "$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' debugstudy0):5900 (password: corebench)"
-  echo ""
-  echo "Connecting .."
-  if [ -z "$2" ]; then 
-    docker exec -it $(docker ps | grep "debugstudy " | cut -c-12) bash
-  else 
-    echo "$2" | docker exec -i $(docker ps | grep "debugstudy " | cut -c-12) bash 
-    exit_code=$?
-  fi
-  
-  exit $exit_code
+if [ $(docker ps | grep -c "$name") -ne 0 ]; then
+  echo "A container named $name is already running. Try reusing it."
+  exit 0
 fi
 
+#if [ $(docker ps | grep -c "debugstudy$subject ") -ne 0 ]; then
+#  echo "An instance of 'debugstudy' is already running ($(docker ps | grep "debugstudy$subject " | cut -c-12))"
+#  echo "You can use VNCViewer from your Desktop or a different terminal window to access: "
+#  echo "$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${name}):5900 (password: corebench)"
+#  echo ""
+#  echo "Connecting .."
+#  if [ -z "$2" ]; then
+#    docker exec -it $(docker ps | grep "debugstudy$subject " | cut -c-12) bash
+#  else
+#    echo "$2" | docker exec -i $(docker ps | grep "debugstudy$subject " | cut -c-12) bash
+#    exit_code=$?
+#  fi
+#
+#  exit $exit_code
+#fi
+
 printf "Running container: "
-docker run -dt --name debugstudy0 -v $(pwd):/shared -p 5900:5900 --dns 8.8.8.8 --dns 8.8.4.4 debugstudy | cut -c-12
+docker run -dt --name ${name} -v $(pwd):/shared --dns 8.8.8.8 --dns 8.8.4.4 debugstudy${subject} | cut -c-12
 echo "Now use VNCViewer from your Desktop or a different terminal window to access: "
-echo "$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' debugstudy0):5900 (password: corebench)"
+echo "$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${name}):5900 (password: corebench)"
 echo
 echo Note: Once the container is removed or broken, any temporary data will be lost!
 echo Use the '/shared'-folder for scripts and data which you would like to keep.  
 echo
 echo Connecting..
-if [ -z "$2" ]; then 
-  docker exec -it $(docker ps | grep "debugstudy " | cut -c-12) bash
+if [ -z "$3" ]; then
+  docker exec -it $name bash
 else 
-  echo "$2" | docker exec -i $(docker ps | grep "debugstudy " | cut -c-12) bash 
+  echo "$3" | docker exec -i $name bash
   exit_code=$?
 fi
 exit $exit_code
